4530b749e285511b48f3d2683404c5e2bebe021a
ivanbiljan <ssbiljan@gmail.com>
2021-07-31 12:47:48 +0200

    Remove Gameiki.Framework.Commands. To be replaced with a CLI parsing library

--- Gameiki/Framework/Commands/ArgumentValidators/Int32Validator.cs
+++
@@ -1,7 +0,0 @@
-﻿using Gameiki.Framework.Commands;
-
-namespace Gameiki.Framework.Commands.ArgumentValidators {
-    public sealed class Int32Validator : IArgument {
-        public object Validate(string input) => int.Parse(input);
-    }
-}
--- Gameiki/Framework/Commands/ArgumentValidators/StringValidator.cs
+++
@@ -1,7 +0,0 @@
-﻿using Gameiki.Framework.Commands;
-
-namespace Gameiki.Framework.Commands.ArgumentValidators {
-    public class StringValidator : IArgument {
-        public object Validate(string input) => input;
-    }
-}
